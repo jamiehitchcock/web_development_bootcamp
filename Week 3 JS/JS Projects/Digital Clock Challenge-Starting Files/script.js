f7ae5a5ccda59e6604fdbf8fb6b9864ae855eb5e
Amytrainer <97535863+Amytrainer@users.noreply.github.com>
2022-01-30 12:18:32 +0000

    Week 3 Digital Clock

--- Week 3 JS/JS Projects/Digital Clock Challenge-Starting Files/script.js
+++ Week 3 JS/JS Projects/Digital Clock Challenge-Starting Files/script.js
@@ -1,8 +1,14 @@
 //Hints
-//function setTime() {
+
+let time =document.getElementById("time");
+function setTime() {
    
-    
-    
-   
-//}
-//setTime();
+    const clock =new Date();
+    let hours=clock.getHours();
+    let mins=clock.getMinutes();
+    let secs=clock.getSeconds();
+    time.innerHTML=hours +": "+mins+":"+secs;
+    setInterval(setTime, 1000);
+}
+//Calling the function
+//console.log(setTime());
